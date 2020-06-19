80ce26215f24bca79dd8e49911ce7320f5ab6930
James Strachan <james.strachan@gmail.com>
2020-06-19 15:08:21 +0100

    fix: switch to go-git
    to simplify the version stream git clone code

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -1,18 +1,18 @@
 package versions
 
 import (
+	"io/ioutil"
+	"os"
 	"strings"
 
 	"github.com/jenkins-x/jx-logging/pkg/log"
 	"github.com/jenkins-x/jx-promote/pkg/common"
-	"github.com/jenkins-x/jx-promote/pkg/githelpers"
 	"github.com/jenkins-x/jx/pkg/config"
 	"github.com/jenkins-x/jx/pkg/gits"
 	"github.com/jenkins-x/jx/pkg/util"
 	"github.com/jenkins-x/jx/pkg/versionstream"
 	"github.com/pkg/errors"
 	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
 // GetDefaultVersionNumber returns the version number for the given kind and name using the default version stream
@@ -34,12 +34,12 @@ func GetVersionNumber(kind versionstream.VersionKind, name, repo, gitRef string,
 
 // CreateVersionResolver creates a new VersionResolver service
 func CreateVersionResolver(versionRepository string, versionRef string, git gits.Gitter, handles util.IOFileHandles) (*versionstream.VersionResolver, error) {
-	versionsDir, err := githelpers.GitCloneToTempDir(git, versionRepository, "")
+	versionsDir, err := GitCloneToDir(versionRepository, "")
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to clone URL %s", versionRepository)
 	}
 	if versionRef != "" && versionRef != "master" && versionRef != "origin/master" && versionRef != "refs/heads/master" {
-		_, err = checkoutRef(versionsDir, versionRepository, versionRef, git)
+		_, err = checkoutRef(versionsDir, versionRef, git)
 		if err != nil {
 			return nil, errors.Wrapf(err, "failed to checkout ref %s in repository %s", versionRef, versionRepository)
 		}
@@ -49,75 +49,50 @@ func CreateVersionResolver(versionRepository string, versionRef string, git gits
 	}, nil
 }
 
-func checkoutRef(wrkDir string, versionRepository string, referenceName string, gitter gits.Gitter) (string, error) {
+// GitCloneToDir clones the git repository to a given or temporary directory
+func GitCloneToDir(gitURL string, dir string) (string, error) {
+	var err error
+	if dir != "" {
+		err = os.MkdirAll(dir, util.DefaultWritePermissions)
+		if err != nil {
+			return "", errors.Wrapf(err, "failed to create directory %s", dir)
+		}
+	} else {
+		dir, err = ioutil.TempDir("", "jx-promote-")
+		if err != nil {
+			return "", errors.Wrap(err, "failed to create temporary directory")
+		}
+	}
+
+	log.Logger().Debugf("cloning %s to directory %s", util.ColorInfo(gitURL), util.ColorInfo(dir))
+
+	_, err = git.PlainClone(dir, false, &git.CloneOptions{
+		URL:               gitURL,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	})
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to clone repository %s to directory: %s", gitURL, dir)
+	}
+	return dir, nil
+}
+
+func checkoutRef(wrkDir string, referenceName string, gitter gits.Gitter) (string, error) {
 	if referenceName == "" || referenceName == "master" || referenceName == "refs/heads/master" {
 		return "", nil
 	}
 
-	if !strings.Contains(referenceName, "/") {
-		if strings.HasPrefix(referenceName, "PR-") {
-			prNumber := strings.TrimPrefix(referenceName, "PR-")
-
-			log.Logger().Debugf("Cloning the Jenkins X versions repo %s with PR: %s to %s", util.ColorInfo(versionRepository), util.ColorInfo(referenceName), util.ColorInfo(wrkDir))
-			return "", shallowCloneGitRepositoryToDir(wrkDir, versionRepository, prNumber, "", gitter)
-		}
-		log.Logger().Debugf("Cloning the Jenkins X versions repo %s with revision %s to %s", util.ColorInfo(versionRepository), util.ColorInfo(referenceName), util.ColorInfo(wrkDir))
-
-		isBranch, err := gits.RefIsBranch(wrkDir, referenceName, gitter)
-		if err != nil {
-			return "", err
-		}
-		if isBranch {
-			err = gitter.Checkout(wrkDir, referenceName)
-			if err != nil {
-				return "", errors.Wrapf(err, "failed to checkout %s of repo: %s in dir %s", referenceName, versionRepository, wrkDir)
-			}
-			return "", nil
-		}
-		err = gitter.Checkout(wrkDir, "FETCH_HEAD")
-		if err != nil {
-			return "", errors.Wrapf(err, "failed to checkout FETCH_HEAD of repo: %s in dir %s", versionRepository, wrkDir)
-		}
-		return "", nil
-	}
-	log.Logger().Infof("Cloning the Jenkins X versions repo %s with ref %s to %s", util.ColorInfo(versionRepository), util.ColorInfo(referenceName), util.ColorInfo(wrkDir))
-	// TODO: Change this to use gitter instead, but need to understand exactly what it's doing first.
-	_, err := git.PlainClone(wrkDir, false, &git.CloneOptions{
-		URL:           versionRepository,
-		ReferenceName: plumbing.ReferenceName(referenceName),
-		SingleBranch:  true,
-		Progress:      nil,
-	})
+	log.Logger().Infof("checking out ref %s", util.ColorInfo(referenceName))
+	err := gitter.Checkout(wrkDir, referenceName)
 	if err != nil {
-		return "", errors.Wrapf(err, "failed to clone reference: %s", referenceName)
-	}
-	return "", err
-}
-
-func shallowCloneGitRepositoryToDir(dir string, gitURL string, pullRequestNumber string, revision string, gitter gits.Gitter) error {
-	if pullRequestNumber != "" {
-		log.Logger().Infof("shallow cloning pull request %s of repository %s to temp dir %s", gitURL,
-			pullRequestNumber, dir)
-		err := gitter.ShallowClone(dir, gitURL, "", pullRequestNumber)
-		if err != nil {
-			return errors.Wrapf(err, "shallow cloning pull request %s of repository %s to temp dir %s\n", gitURL,
-				pullRequestNumber, dir)
-		}
-	} else if revision != "" {
-		log.Logger().Infof("shallow cloning revision %s of repository %s to temp dir %s", gitURL,
-			revision, dir)
-		err := gitter.ShallowClone(dir, gitURL, revision, "")
-		if err != nil {
-			return errors.Wrapf(err, "shallow cloning revision %s of repository %s to temp dir %s\n", gitURL,
-				revision, dir)
-		}
-	} else {
-		log.Logger().Infof("shallow cloning master of repository %s to temp dir %s", gitURL, dir)
-		err := gitter.ShallowClone(dir, gitURL, "", "")
-		if err != nil {
-			return errors.Wrapf(err, "shallow cloning master of repository %s to temp dir %s\n", gitURL, dir)
+		// lets see if its a tag
+		if strings.HasPrefix(referenceName, "v") {
+			referenceName = "tags/" + referenceName
+			log.Logger().Infof("checking out ref %s", util.ColorInfo(referenceName))
+			err = gitter.Checkout(wrkDir, referenceName)
 		}
 	}
-
-	return nil
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to clone ref %s", referenceName)
+	}
+	return "", nil
 }
