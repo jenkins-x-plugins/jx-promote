67c9e4edcc3453f7fc8fc1ee9f7bfe2a62857328
James Strachan <james.strachan@gmail.com>
2020-06-22 10:13:34 +0100

    fix: add promote command

--- promote.sh
+++ promote.sh
@@ -2,4 +2,6 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
+jx step create pr regex --regex "const JXPromoteVersion = \"(?P<version>.*)\"" --version ${VERSION} --files cmd/enhance-pipeline/cmd/enhancePipeline.go --repo hhttps://github.com/cloudbees/jx-app-cb-remote.git
+
 jx step create pr go --name github.com/jenkins-x/jx-promote --version ${VERSION} --build "make build" --repo https://github.com/jenkins-x/jxl.git
