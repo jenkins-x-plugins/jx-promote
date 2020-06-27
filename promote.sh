aa6c57643029e477c0bae8d10db95ac77ab5fbd0
James Strachan <james.strachan@gmail.com>
2020-06-27 07:06:49 +0100

    fix: promote image on release

--- promote.sh
+++ promote.sh
@@ -5,3 +5,6 @@ echo "promoting the new version ${VERSION} to downstream repositories"
 jx step create pr regex --regex 'const JXPromoteVersion = "(?P<version>.*)"' --version ${VERSION} --files cmd/enhance-pipeline/cmd/enhancePipeline.go --repo https://github.com/cloudbees/jx-app-cb-remote.git
 
 jx step create pr regex --regex 'version: (.*)' --version ${VERSION} --files docker/gcr.io/jenkinsxio-labs-private/jx-promote.yml --repo https://github.com/jenkins-x/jxr-versions.git
+
+jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio-labs-private/jx-gitops:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jxl-base-image-jx.git
+
