9f3f9aea4d5142bc2d85f6e9bfe9fc4670cd07cd
James Strachan <james.strachan@gmail.com>
2020-07-02 07:16:12 +0100

    fix: promote

--- promote.sh
+++ promote.sh
@@ -2,9 +2,7 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
-jx step create pr regex --regex 'const JXPromoteVersion = "(?P<version>.*)"' --version ${VERSION} --files cmd/enhance-pipeline/cmd/enhancePipeline.go --repo https://github.com/cloudbees/jx-app-cb-remote.git
+jx step create pr regex --regex '\s+SecretVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/version.go --repo https://github.com/jenkins-x/jx-cli.git
 
 jx step create pr regex --regex 'version: (.*)' --version ${VERSION} --files docker/gcr.io/jenkinsxio-labs-private/jx-promote.yml --repo https://github.com/jenkins-x/jxr-versions.git
-
-jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio-labs-private/jx-gitops:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jxl-base-image-jx.git
 
