d03cec80dfc5bba15f5b55bc0fa1b1cac41f4e9f
James Strachan <james.strachan@gmail.com>
2020-07-02 08:20:32 +0100

    fix: promote

--- promote.sh
+++ promote.sh
@@ -2,7 +2,7 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
-jx step create pr regex --regex '\s+SecretVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/version.go --repo https://github.com/jenkins-x/jx-cli.git
+jx step create pr regex --regex '\s+PromoteVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git
 
 jx step create pr regex --regex 'version: (.*)' --version ${VERSION} --files docker/gcr.io/jenkinsxio-labs-private/jx-promote.yml --repo https://github.com/jenkins-x/jxr-versions.git
 
