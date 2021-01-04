f92428aec22fe1eca36c83db09f7b8ca2b050993
James Strachan <james.strachan@gmail.com>
2021-01-04 14:32:49 +0000

    fix: lets add an --all mode for promotion
    so we can promote to both Auto and Manual environments

--- pkg/promote/promote_integration_test.go
+++ pkg/promote/promote_integration_test.go
@@ -44,6 +44,109 @@ func TestPromoteIntegrationMakefileKpt(t *testing.T) {
 	})
 }
 
+func TestPromoteHelmfileAllAutomaticAndManual(t *testing.T) {
+	version := "1.2.3"
+	appName := "myapp"
+	ns := "jx"
+
+	runner := NewFakeRunnerWithGitClone()
+
+	_, po := promote.NewCmdPromote()
+	name := "promote-all"
+	po.DisableGitConfig = true
+	po.Application = appName
+	po.Version = version
+	po.All = true
+
+	po.NoPoll = true
+	po.BatchMode = true
+	po.GitKind = "fake"
+	po.CommandRunner = runner.Run
+	po.AppGitURL = "https://github.com/myorg/myapp.git"
+
+	targetFullName := "jenkins-x/default-environment-helmfile"
+
+	devEnv := jxtesthelpers.CreateTestDevEnvironment(ns)
+	devEnv.Spec.Source.URL = "https://github.com/jenkins-x-labs-bdd-tests/jx3-kubernetes-jenkins"
+
+	kubeObjects := []runtime.Object{
+		&corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: ns,
+				Labels: map[string]string{
+					"tag":  "",
+					"team": "jx",
+					"env":  "dev",
+				},
+			},
+		},
+	}
+	jxObjects := []runtime.Object{
+		devEnv,
+		&v1.Environment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "staging",
+				Namespace: ns,
+			},
+			Spec: v1.EnvironmentSpec{
+				Label:             "Staging",
+				Namespace:         "jx-staging",
+				PromotionStrategy: v1.PromotionStrategyTypeAutomatic,
+				Order:             100,
+				Kind:              v1.EnvironmentKindTypePermanent,
+			},
+		},
+		&v1.Environment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "production",
+				Namespace: ns,
+			},
+			Spec: v1.EnvironmentSpec{
+				Label:             "Production",
+				Namespace:         "jx-production",
+				PromotionStrategy: v1.PromotionStrategyTypeManual,
+				Order:             200,
+				Kind:              v1.EnvironmentKindTypePermanent,
+			},
+		},
+	}
+
+	po.KubeClient = fake.NewSimpleClientset(kubeObjects...)
+	po.JXClient = v1fake.NewSimpleClientset(jxObjects...)
+	po.Namespace = ns
+	po.Build = "1"
+	po.Pipeline = "myorg/myapp/master"
+	po.DevEnvContext.VersionResolver = jxtesthelpers.CreateTestVersionResolver(t)
+
+	err := po.Run()
+	require.NoError(t, err, "failed test %s s", name)
+
+	scmClient := po.ScmClient
+	require.NotNil(t, scmClient, "no ScmClient created")
+	ctx := context.Background()
+
+	for prNumber := 1; prNumber < 3; prNumber++ {
+		pr, _, err := scmClient.PullRequests.Find(ctx, targetFullName, prNumber)
+		require.NoError(t, err, "failed to find repository %s number %d", targetFullName, prNumber)
+		assert.NotNil(t, pr, "nil pr %s for %s", targetFullName, prNumber)
+
+		t.Logf("created PullRequest %s #%d", pr.Link, prNumber)
+		t.Logf("PR title: %s", pr.Title)
+		t.Logf("PR body: %s", pr.Body)
+	}
+	// lets assert we have a PipelineActivity...
+	paList, err := po.JXClient.JenkinsV1().PipelineActivities(ns).List(context.TODO(), metav1.ListOptions{})
+	require.NoError(t, err, "failed to load PipelineActivity resources in namespace %s", ns)
+	require.Len(t, paList.Items, 1, "should have a PipelineActivity in namespace %s", ns)
+	pa := paList.Items[0]
+
+	data, err := yaml.Marshal(pa)
+	require.NoError(t, err, "failed to marshal PipelineActivity")
+
+	t.Logf("got PipelineActivity %s\n", string(data))
+	assert.Equal(t, v1.ActivityStatusTypeSucceeded, pa.Spec.Status, "pipelineActivity.Spec.Status")
+}
+
 // AssertPromoteIntegration asserts the test cases work
 func AssertPromoteIntegration(t *testing.T, testCases ...PromoteTestCase) {
 	version := "1.2.3"
