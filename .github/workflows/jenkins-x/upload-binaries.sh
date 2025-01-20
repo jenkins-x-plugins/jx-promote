fcdf297837fc576146125bf06a95ded51d4b21c3
Mårten Svantesson <Marten.Svantesson@ticket.se>
2025-01-20 17:55:50 +0100

    fix: release
    Signed-off-by: Mårten Svantesson <Marten.Svantesson@ticket.se>

--- .github/workflows/jenkins-x/upload-binaries.sh
+++ .github/workflows/jenkins-x/upload-binaries.sh
@@ -5,8 +5,6 @@ echo current git configuration
 
 # See https://github.com/actions/checkout/issues/766
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
-git config --global --get user.name
-git config --global --get user.email
 
 echo "setting git user"
 
@@ -15,8 +13,6 @@ git config --global user.email "jenkins-x@googlegroups.com"
 
 git add * || true
 git commit -a -m "chore: release $VERSION" --allow-empty
-git tag -fa v$VERSION -m "Release version $VERSION"
-git push origin v$VERSION
 
 export BRANCH=$(git rev-parse --abbrev-ref HEAD)
 export BUILDDATE=$(date)
