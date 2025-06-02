616b80a1808c4ee2a9eb370ee5d476f2bdb08d80
Mårten Svantesson <Marten.Svantesson@ticket.se>
2025-06-02 20:58:19 +0200

    fix: release
    Signed-off-by: Mårten Svantesson <Marten.Svantesson@ticket.se>

--- .github/workflows/jenkins-x/changelog.sh
+++ .github/workflows/jenkins-x/changelog.sh
@@ -2,10 +2,11 @@
 
 git config --global --add safe.directory /github/workspace
 
+git add * && git commit -a -m "chore: release $VERSION" || echo No change
+
 export PULL_BASE_SHA=$(git rev-parse HEAD)
 
-git add * || true
-git commit -a -m "chore: release $VERSION" --allow-empty
+
 
 # See https://github.com/actions/checkout/issues/766
 jx changelog create --verbose --version=$VERSION --rev=$PULL_BASE_SHA --output-markdown=changelog.md
