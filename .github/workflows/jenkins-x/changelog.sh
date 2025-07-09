ee9e8d9c307b34b3651b18e50f2b0aa63e548e08
Mårten Svantesson <Marten.Svantesson@ticket.se>
2025-07-09 09:54:34 +0200

    docs: move comment to correct place
    Signed-off-by: Mårten Svantesson <Marten.Svantesson@ticket.se>

--- .github/workflows/jenkins-x/changelog.sh
+++ .github/workflows/jenkins-x/changelog.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -x
 
+# See https://github.com/actions/checkout/issues/766
 git config --global --add safe.directory /github/workspace
 
 git add * && git commit -a -m "chore: release $VERSION" || echo No change
 
 export PULL_BASE_SHA=$(git rev-parse HEAD)
 
-
-
-# See https://github.com/actions/checkout/issues/766
 jx changelog create --verbose --version=$VERSION --rev=$PULL_BASE_SHA --output-markdown=changelog.md
